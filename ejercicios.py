6b8500c6bfc5af6d2306bb6bb2b5585197da594a
JulianDavidMG <jdmartinezg2@academia.usbbog.edu.co>
2025-08-25 18:54:45 -0500

    5 Ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,28 @@
+# 1. Operaciones con enteros
+a = 12
+b = 5
+suma = a + b
+producto = a * b
+print("Suma:", suma, "Producto:", producto)
+
+# 2. Operaciones con flotantes
+pi = 3.1416
+w = 2.3
+l = 5.6
+z = w * l
+print("pi:", pi ,"mutiplicación:", z)
+
+# 3. Cadenas y bytes
+palabra = "Python"
+letra = palabra[0]
+print("Primera letra:", letra, ", Palabra:", palabra)
+
+# 4. Booleanos
+es_mayor = a > b
+print("¿a > b?", es_mayor,)
+
+# 5. Lista
+numeros = [1, 2, 3, 4, 5]
+print("Lista:", numeros)
+for n in numeros:
+    print("Elemento:", n)
